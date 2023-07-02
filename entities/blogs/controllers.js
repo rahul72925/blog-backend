57b0bd3e9e58663fd7a41b8db7d7792099c3ef46
rahul72925 <rahulkhatri72925@gmail.com>
2023-07-02 14:11:32 +0530

    feat: create api for update blog

--- entities/blogs/controllers.js
+++ entities/blogs/controllers.js
@@ -25,3 +25,44 @@ export const createBlog = async (req, res) => {
     });
   }
 };
+
+export const updateBlog = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const blogId = req.query.blogId;
+
+    const {
+      title,
+      tags,
+      categories,
+      post,
+      isDraft,
+      isAllowComments,
+      isPublished,
+      isArchived,
+    } = req.body;
+
+    const returning = await sql`
+  UPDATE blogs set title = ${title}, 
+                  tags = ${tags} , 
+                  categories = ${categories}, 
+                  post = ${post}, 
+                  is_draft = ${isDraft}, 
+                  is_allow_comments = ${isAllowComments},
+                  is_published = ${isPublished},
+                  is_archived = ${isArchived} where id = ${blogId} and user_id = ${userId};
+  `;
+
+    res.status(200).json({
+      success: true,
+      message: "Updated successfully",
+    });
+  } catch (error) {
+    console.log("update blog error", error);
+
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+};
--- entities/blogs/routes.js
+++ entities/blogs/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBlog } from "./controllers.js";
+import { createBlog, updateBlog } from "./controllers.js";
 import { validateJWT } from "../../utils/index.js";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/create-blog", validateJWT, createBlog);
+blogRouter.post("/update-blog", validateJWT, updateBlog);
 
 export { blogRouter };
